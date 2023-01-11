448e4381570d010c66e3b538bc3e448959e851ab
Bhuvan MC <bhuvanmc@MBP-DJQ05N-BMC.local>
2023-01-11 13:00:05 +0530

    Added some variables and fucnctions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,23 @@
+
+var greeting="hello"
+fun getGreeting1() = greeting
+fun getGreeting2():String{
+    return "hi"
+}
+fun printAlert(type:String, description:String)=println("$type $description")
 fun main(args: Array<String>) {
-    println("Hello World!")
-    println("Hello")
+    println("Welcome to Alerts")
+    val type:String = "Query_Overload"
+    var user:String = "User1"
+    var cost=10000
+    var avg_cost=15000
+
+    var level = if(cost>avg_cost){ "critical" }else{ "intermediate"}
+    var description:String="$user $cost $level"
+
+    println("${getGreeting1()} ${getGreeting2()}")
+    printAlert(type, description)
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+//    println("Program arguments: ${args.joinToString()}")
 }
